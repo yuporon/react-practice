176ff081593c8bb358daff6e951f63a4cca3ffc1
yuporon <ftb.y.m.kashima.07@gmail.com>
2024-04-15 12:26:53 +0900

    TodoApp1: タスクがない場合の条件分岐

--- src/ExerciseComponents/TodoApp/TodoApp.tsx
+++ src/ExerciseComponents/TodoApp/TodoApp.tsx
@@ -2,15 +2,21 @@ import { Box, VStack } from "@chakra-ui/react";
 import { TodoListFilter } from "./TodoListFilter/TodoListFilter";
 import { useTodoListFilter } from "./TodoListFilter/useTodoListFilter";
 import { TodoList } from "./TodoList/TodoList";
+import { Todo } from "./Todo.type";
 
 export function TodoApp() {
+  const todoList: Todo[] = []
   const todoListFilter = useTodoListFilter();
 
   return (
     <Box as="main" p={4} maxWidth={300} mx="auto">
       <VStack gap={4} align="start">
         <TodoListFilter {...todoListFilter} />
-        <TodoList todoList={[]} />
+        {todoList.length === 0 ? (
+          <p>タスクがありません。</p>
+        ) : (
+          <TodoList todoList={todoList} />
+        )}
       </VStack>
     </Box>
   );

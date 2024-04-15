03c3fde81855150a995a48dde93bdbade546146f
ryosuke.tokunaga <ryosuke.tokunaga@medley.jp>
2024-04-15 10:22:24 +0900

    feat: index をいい感じにしておく

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,31 @@
-import { useState } from "react";
-import { Flex, Box, Link, useColorModeValue } from "@chakra-ui/react";
+import { Box, Center, Link, Stack } from "@chakra-ui/react";
 
 export default function Page() {
-  const [animation, setAnimation] = useState("");
-
   return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Box
-        p={5}
-        shadow="md"
-        borderWidth="1px"
-        borderRadius="md"
-        bg={useColorModeValue("white", "gray.700")}
-        animation={animation}
-      >
-        <Flex justifyContent="center">
-          <Link
-            href="/exercise/question-1"
-            _hover={{ color: "blue.500", textDecoration: "underline" }}
-            fontSize="xl"
-          >
-            問題 1 に進む
-          </Link>
-        </Flex>
-      </Box>
-    </Flex>
+    <Center pt={20}>
+      <Stack spacing={4}>
+        <Box fontSize="2xl" fontWeight="bold">
+          ここから練習問題を選択してください
+        </Box>
+        <Stack width={500}>
+          <Box>
+            <Link
+              href="/exercise/question-1"
+              _hover={{ color: "blue.500", textDecoration: "underline" }}
+            >
+              練習問題
+            </Link>
+          </Box>
+          <Box>
+            <Link
+              href="/exercise/todo-app"
+              _hover={{ color: "blue.500", textDecoration: "underline" }}
+            >
+              TodoList 演習
+            </Link>
+          </Box>
+        </Stack>
+      </Stack>
+    </Center>
   );
 }
